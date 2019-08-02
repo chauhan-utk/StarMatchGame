aa974ebeefb6c8895d54b58ae1d09baecd5e0eec
Utkarsh Chauhan <utchauha@microsoft.com>
2019-08-02 22:10:31 +0530

    Updates

---
+++ precache-manifest.0a62f2f3ec897788d35fe10a6dab42d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9797468dfe5fa94b78eadbc7a0f5b7d4",
+    "url": "/StarMatchGame/index.html"
+  },
+  {
+    "revision": "47b2640d9c264503f382",
+    "url": "/StarMatchGame/static/css/main.69ab3ecf.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/StarMatchGame/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "47b2640d9c264503f382",
+    "url": "/StarMatchGame/static/js/main.4fa2dc80.chunk.js"
+  },
+  {
+    "revision": "6c63de72c36205efe523",
+    "url": "/StarMatchGame/static/js/runtime~main.ff845727.js"
+  }
+]);
